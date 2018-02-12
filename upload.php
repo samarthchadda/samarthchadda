3b31374696fade7b919d97b73c80c53ddba18aa7
samarthchadda <33507821+samarthchadda@users.noreply.github.com>
2018-02-12 07:58:33 +0530

    create upload.php

---
+++ upload.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<body background="1112.jpg">
+Thank you <h1><?php echo $_POST["fname"]; ?></h1><br>
+<h3>Your email address is:</h3> <?php echo $_POST["Email"]; ?>
+<br><br><br><h1 style="font-size: 35px">YOUE RESPONSE IS SUBMITTED TO SAMARTH TECH.</h1>
+</body>
+</html>
